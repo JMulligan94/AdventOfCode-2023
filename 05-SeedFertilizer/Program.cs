ccb5036e2f68cc1caab9b853505eced008c6fe71
JMulligan94 <j.mulligan94@hotmail.co.uk>
2023-12-10 00:26:02 +0000

    Day 5 Part 1 and Day 6

---
+++ 05-SeedFertilizer/Program.cs
@@ -0,0 +1,77 @@
+﻿
+var isTesting = true;
+var inputFile = isTesting ? "test.txt" : "input.txt";
+
+var seeds = new List<ulong>();
+var seedRanges = new List<(ulong, ulong)>();
+
+var maps = new List<List<Mapping>>();
+var currentIndex = -1;
+foreach (var inputLine in File.ReadLines(inputFile))
+{
+    if (inputLine == string.Empty)
+        continue;
+    
+    if (inputLine.StartsWith("seeds:"))
+    {
+        seeds = inputLine.Substring("seeds: ".Length)
+            .Split(' ')
+            .Select(ulong.Parse)
+            .ToList();
+
+        for (var seedIndex = 0; seedIndex < seeds.Count; seedIndex+=2)
+        {
+            seedRanges.Add((seeds[seedIndex], seeds[seedIndex+1]));
+        }
+        continue;
+    }
+
+    if (char.IsLetter(inputLine[0]))
+    {
+        // Start of new map
+        currentIndex++;
+        maps.Add(new List<Mapping>());
+        continue;
+    }
+
+    var mappingInfo = inputLine.Split(' ');
+    maps.Last().Add(new Mapping()
+    {
+        Source = ulong.Parse(mappingInfo[1]),
+        Dest = ulong.Parse(mappingInfo[0]),
+        Size = ulong.Parse(mappingInfo[2]),
+    });
+}
+
+var partOneLocations = new List<ulong>();
+foreach (var seed in seeds)
+{
+    var seedValue = seed;
+    foreach(var mappingSet in maps)
+    {
+        var matchingMapping = mappingSet
+            .FirstOrDefault(mapping => seedValue >= mapping.SourceRange.Item1
+                                       && seedValue <= mapping.SourceRange.Item2);
+
+        if (matchingMapping != null)
+            seedValue += matchingMapping.Offset;
+    }
+    
+    Console.WriteLine($"Seed: {seed}, Location {seedValue}");
+    partOneLocations.Add(seedValue);
+}
+
+Console.WriteLine("\n=== Part One ===");
+Console.WriteLine($"Lowest location number is: {partOneLocations.Min()}");
+
+class Mapping
+{
+    public ulong Source { get; init; }
+    public ulong Dest { get; init; }
+    public ulong Size { get; init; }
+
+    public ulong Offset => Dest - Source;
+
+    public (ulong,ulong) SourceRange => (Source, Source + Size - 1);
+    public (ulong,ulong) DestRange => (Dest, Dest + Size - 1);
+} 
---
+++ 06-WaitForIt/Program.cs
@@ -0,0 +1,58 @@
+﻿
+var isTesting = false;
+var inputFile = isTesting ? "test.txt" : "input.txt";
+
+var lines = File.ReadAllLines(inputFile);
+var times  = lines[0].Split(':')[1]
+    .Split(' ')
+    .Where(s => !string.IsNullOrEmpty(s))
+    .Select(int.Parse)
+    .ToList();
+
+var records = lines[1].Split(':')[1]
+    .Split(' ')
+    .Where(s => !string.IsNullOrEmpty(s))
+    .Select(int.Parse)
+    .ToList();
+
+var recordBreakingHoldTimes = new List<int>();
+for (var raceIndex = 0; raceIndex < times.Count; ++raceIndex)
+{
+    var raceTime = times[raceIndex];
+    var recordDistance = records[raceIndex];
+
+    var recordBreakingCount = 0;
+    for (var msHeld = 0; msHeld < raceTime; ++msHeld)
+    {
+        var speed = msHeld;
+        var distance = speed * (raceTime - msHeld);
+
+        Console.WriteLine($"[Race {raceIndex}] Holding button for {msHeld}ms: {distance}mm");
+        if (distance > recordDistance)
+            recordBreakingCount++;
+    }
+    
+    recordBreakingHoldTimes.Add(recordBreakingCount);
+}
+
+Console.WriteLine("\n=== Part One ===");
+Console.WriteLine($"Product of record breaking counts is: {recordBreakingHoldTimes.Aggregate((acc, result) => result *= acc)}");
+
+var partTwoRecordStr = records.Aggregate("", (current, record) => current + record.ToString());
+var partTwoTimeStr = times.Aggregate("", (current, time) => current + time.ToString());
+
+var partTwoTime = ulong.Parse(partTwoTimeStr);
+var partTwoRecord = ulong.Parse(partTwoRecordStr);
+
+var partTwoCount = 0;
+for (ulong msHeld = 0; msHeld < partTwoTime; ++msHeld)
+{
+    var speed = msHeld;
+    var distance = speed * (partTwoTime - msHeld);
+
+    if (distance > partTwoRecord)
+        partTwoCount++;
+}
+
+Console.WriteLine("=== Part Two ===");
+Console.WriteLine($"Number of record breaking values: {partTwoCount}");
